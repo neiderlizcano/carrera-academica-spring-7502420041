aeb952f32bf4730ffdc5d33e5a9b26980981de23
Neider Lizcano <nlizcanos@unicartagena.edu.co>
2026-05-24 14:59:32 -0500

    chore: crear proyecto base Spring Boot MVC

---
+++ src/main/java/com/neider/carreraacademica/CarreraAcademicaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.neider.carreraacademica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarreraAcademicaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarreraAcademicaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neider/carreraacademica/CarreraAcademicaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neider.carreraacademica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarreraAcademicaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
